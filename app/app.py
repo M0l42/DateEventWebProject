ae0edd4eee7e1406c6b3618c09af333216668698
Nathan Boukobza <boukobza.nathan@gmail.com>
2022-11-27 18:56:31 +0100

    removing sql db and putting a json-type sys

--- app/app.py
+++ app/app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from EventStore.event_store import DatetimeEventStore
+from .config import JSON_PATH
+import json
+import os
 
 app = FastAPI()
-event_store = DatetimeEventStore()
+
+with open(JSON_PATH, 'r') as json_read:
+    event_store = DatetimeEventStore(json.load(json_read))
+
+
+def commit():
+    with open(JSON_PATH, 'w') as json_write:
+        json.dump(event_store.events, json_write, indent=4, default=str)
 
 
 @app.get("/", tags=['ROOT'])
@@ -21,6 +31,29 @@ async def get_events(start, end):
 
 
 @app.post('/events', tags=['EVENTS'])
-async def add_events(event:dict):
+async def add_events(event: dict):
     event_store.store_event(event['date'], event['event'])
+    commit()
     return {"data": "An event has been added !"}
+
+
+@app.put('/events/{id}', tags=['EVENTS'])
+async def update_events(id:int, body: dict):
+    event_store.update_event(id, body)
+    commit()
+    return {"data": "An event has been added !"}
+
+
+@app.delete("/events/{id}", tags=['EVENTS'])
+async def delete_event(id: int):
+    event_store.delete_event([event_store.events[id]])
+    commit()
+    return {"data": f"events with id {id} has been deleted"}
+
+
+@app.delete("/events/multi/{start}_{end}", tags=['EVENTS'])
+async def delete_multiple_event(start, end):
+    res = event_store.get_events(start, end)
+    event_store.delete_event(res)
+    commit()
+    return {"data": f"events with id {id} has been deleted"}
--- app/config.py
+++ app/config.py
@@ -1,10 +1,4 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import os
 
-
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/python_db"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+JSON_PATH = DIR_PATH + "/data.json"
