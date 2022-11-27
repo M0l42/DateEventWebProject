eeb7e5d0adbdcee1be98eda9097d6da8d4f6f53b
Nathan Boukobza <boukobza.nathan@gmail.com>
2022-11-27 16:27:25 +0100

    add GET and POST

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from EventStore.event_store import DatetimeEventStore
 
 app = FastAPI()
+event_store = DatetimeEventStore()
 
 
 @app.get("/", tags=['ROOT'])
@@ -8,6 +10,17 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}", tags=['HELLO'])
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/events", tags=['EVENTS'])
+async def get_all_events():
+    return {"data": event_store.events}
+
+
+@app.get("/events/{start}_{end}", tags=['EVENTS'])
+async def get_events(start, end):
+    return {"data": event_store.get_events(start, end)}
+
+
+@app.post('/events', tags=['EVENTS'])
+async def add_events(event:dict):
+    event_store.store_event(event['date'], event['event'])
+    return {"data": "An event has been added !"}
